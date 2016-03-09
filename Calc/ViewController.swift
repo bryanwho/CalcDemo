65668605e85f3871f67e00b9643a2d62e3e2079b
bryan <bryan@beginnerprogrammers.nyc>
2016-03-09 11:09:36 -0500

    Initial Commit

---
+++ Calc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calc
+//
+//  Created by Bryan Okeke on 3/9/16.
+//  Copyright © 2016 Beginner Programmers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
